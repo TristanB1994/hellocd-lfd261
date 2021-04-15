4b06d9ceda6f9897dbd4ff50067abdba2cdcedd0
Tristan <t.blandford.dev@gmail.com>
2021-04-15 10:06:47 -0400

    added web dependency to pom.xml, added root request mapping to application

--- src/main/java/com/example/helloCD/HelloCdApplication.java
+++ src/main/java/com/example/helloCD/HelloCdApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloCdApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "Hello world";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloCdApplication.class, args);
 	}
